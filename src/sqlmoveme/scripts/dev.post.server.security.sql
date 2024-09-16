822384e0ba7b629e5ca536d375ae0d9c69c46117
John Folberth <johnfolberth@gmail.com>
2024-09-16 13:32:18 -0400

    feat: Replace post-deployment security script with new scripts for user and role management

--- src/sqlmoveme/scripts/dev.post.server.security.sql
+++ src/sqlmoveme/scripts/dev.post.server.security.sql
@@ -2,8 +2,5 @@ Use master
 CREATE LOGIN [sqlmoveme_dev_admins@MngEnvMCAP100965.onmicrosoft.com] FROM EXTERNAL PROVIDER
 CREATE USER  [sqlmoveme_dev_admins@MngEnvMCAP100965.onmicrosoft.com] FROM LOGIN  [sqlmoveme_dev_admins@MngEnvMCAP100965.onmicrosoft.com]
 GO
-Use sqlmoveme
-ALTER ROLE [dbmanager] ADD MEMBER [sqlmoveme_dev_admins@MngEnvMCAP100965.onmicrosoft.com] 
-ALTER ROLE [loginmanager] ADD MEMBER [sqlmoveme_dev_admins@MngEnvMCAP100965.onmicrosoft.com] 
-GO
+
 
---
+++ src/sqlmoveme/scripts/dev.post.sqlmoveme.security.sql
@@ -0,0 +1,4 @@
+Use [sqlmoveme]
+ALTER ROLE [dbmanager] ADD MEMBER [sqlmoveme_dev_admins@MngEnvMCAP100965.onmicrosoft.com] 
+ALTER ROLE [loginmanager] ADD MEMBER [sqlmoveme_dev_admins@MngEnvMCAP100965.onmicrosoft.com] 
+GO
