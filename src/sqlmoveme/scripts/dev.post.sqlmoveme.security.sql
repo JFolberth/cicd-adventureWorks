20f1aebf555d669798ce728b5abebe6f02831c82
John Folberth <johnfolberth@gmail.com>
2024-09-16 15:21:09 -0400

    fix: Add BEGIN/END blocks for user creation in security scripts for dev and tst environments

--- src/sqlmoveme/scripts/dev.post.sqlmoveme.security.sql
+++ src/sqlmoveme/scripts/dev.post.sqlmoveme.security.sql
@@ -1,14 +1,17 @@
 -- SECURITY SECTION
 USE [sqlmoveme]
-IF NOT Exists(SELECT * FROM sys.database_principals WHERE name = 'sqlmoveme_dev_admins')  
-    CREATE USER  [sqlmoveme_dev_admins] FROM EXTERNAL PROVIDER
-    EXEC sp_addrolemember 'db_owner','sqlmoveme_dev_admins';
-GO
+
+IF NOT Exists(SELECT * FROM sys.database_principals WHERE name = 'sqlmoveme_dev_admins') 
+    BEGIN  
+        CREATE USER  [sqlmoveme_dev_admins] FROM EXTERNAL PROVIDER
+        EXEC sp_addrolemember 'db_owner','sqlmoveme_dev_admins';
+    END
 
 -- DROP AND RELOAD DATA SECTION
-USE [sqlmoveme]
-DROP TABLE IF EXISTS [SalesLT].[Address];
-INSERT INTO [SalesLT].[Address]VALUES('Apartment 5A','129 W. 81st St.','New York','NY','USA','10001',newid(),SYSDATETIME())
-INSERT INTO [SalesLT].[Address]VALUES('2311 North Los Robles Avenue','','Pasadena','CA','USA','91001',newid(),SYSDATETIME())
-INSERT INTO [SalesLT].[Address]VALUES('742 Evergreen Terrace','','Springfield','IL','USA','65619',newid(),SYSDATETIME())
+BEGIN
+    DROP TABLE IF EXISTS [SalesLT].[Address];
+    INSERT INTO [SalesLT].[Address]VALUES('Apartment 5A','129 W. 81st St.','New York','NY','USA','10001',newid(),SYSDATETIME())
+    INSERT INTO [SalesLT].[Address]VALUES('2311 North Los Robles Avenue','','Pasadena','CA','USA','91001',newid(),SYSDATETIME())
+    INSERT INTO [SalesLT].[Address]VALUES('742 Evergreen Terrace','','Springfield','IL','USA','65619',newid(),SYSDATETIME())
+END
 GO
--- src/sqlmoveme/scripts/tst.post.sqlmoveme.security.sql
+++ src/sqlmoveme/scripts/tst.post.sqlmoveme.security.sql
@@ -1,6 +1,8 @@
 -- SECURITY SECTION
 USE [sqlmoveme]
-IF NOT Exists(SELECT * FROM sys.database_principals WHERE name = 'sqlmoveme_tst_admins')  
-    CREATE USER  [sqlmoveme_tst_admins] FROM EXTERNAL PROVIDER
-    EXEC sp_addrolemember 'db_owner','sqlmoveme_tst_admins';
+IF NOT Exists(SELECT * FROM sys.database_principals WHERE name = 'sqlmoveme_tst_admins')
+    BEGIN  
+        CREATE USER  [sqlmoveme_tst_admins] FROM EXTERNAL PROVIDER
+        EXEC sp_addrolemember 'db_owner','sqlmoveme_tst_admins';
+    END
 GO
