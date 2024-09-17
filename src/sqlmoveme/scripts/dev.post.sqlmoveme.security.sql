9c5a9899b22e116887c508eb2454a2af0b74e539
John Folberth <johnfolberth@gmail.com>
2024-09-17 13:30:37 -0400

    fix: Update security scripts to create user and clean up obsolete files

--- src/sqlmoveme/scripts/dev.post.sqlmoveme.security.sql
+++ src/sqlmoveme/scripts/dev.post.sqlmoveme.security.sql
@@ -1,5 +1,9 @@
+USE [sqlmoveme]
+
 IF NOT Exists(SELECT * FROM sys.database_principals WHERE name = 'sqlmoveme_dev_admins') 
     BEGIN  
         CREATE USER  [sqlmoveme_dev_admins] FROM EXTERNAL PROVIDER
     END
 EXEC sp_addrolemember 'db_owner','sqlmoveme_dev_admins';
+
+GO
