eafaa1fb8c798b27d1e0972bb48dca97c9ca4929
John Folberth <johnfolberth@gmail.com>
2024-09-17 13:46:16 -0400

    feat: Add testing stage to Azure pipeline and implement security scripts for tst environment

---
+++ src/sqlmoveme/scripts/tst.post.script.sql
@@ -0,0 +1,2 @@
+:r tst.post.sqlmoveme.security.sql
+:r tst.post.sqlmoveme.seed.sql
---
+++ src/sqlmoveme/scripts/tst.post.sqlmoveme.security.sql
@@ -0,0 +1,9 @@
+USE [sqlmoveme]
+
+IF NOT Exists(SELECT * FROM sys.database_principals WHERE name = 'sqlmoveme_tst_admins') 
+    BEGIN  
+        CREATE USER  [sqlmoveme_dev_admins] FROM EXTERNAL PROVIDER
+    END
+EXEC sp_addrolemember 'db_owner','sqlmoveme_tst_admins';
+
+GO
