3ae27a08148bcd2b3d35f1e1fd7842cfd10b8b01
John Folberth <johnfolberth@gmail.com>
2024-09-17 11:11:02 -0400

    fix: Simplify user creation and role assignment in security script

--- src/sqlmoveme/scripts/dev.post.sqlmoveme.security.sql
+++ src/sqlmoveme/scripts/dev.post.sqlmoveme.security.sql
@@ -1,10 +1,5 @@
-USE [sqlmoveme]
-
 IF NOT Exists(SELECT * FROM sys.database_principals WHERE name = 'sqlmoveme_dev_admins') 
     BEGIN  
         CREATE USER  [sqlmoveme_dev_admins] FROM EXTERNAL PROVIDER
-        EXEC sp_addrolemember 'db_owner','sqlmoveme_dev_admins';
     END
-
-
-GO
+EXEC sp_addrolemember 'db_owner','sqlmoveme_dev_admins';
--- src/sqlmoveme/scripts/dev.post.sqlmoveme.seed.sql
+++ src/sqlmoveme/scripts/dev.post.sqlmoveme.seed.sql
@@ -1,9 +1,6 @@
-USE [sqlmoveme]
--- DROP AND RELOAD DATA SECTION
 BEGIN
     DELETE FROM [SalesLT].[Address];
     INSERT INTO [SalesLT].[Address]VALUES('Apartment 5A','129 W. 81st St.','New York','NY','USA','10001',newid(),SYSDATETIME())
     INSERT INTO [SalesLT].[Address]VALUES('2311 North Los Robles Avenue','','Pasadena','CA','USA','91001',newid(),SYSDATETIME())
     INSERT INTO [SalesLT].[Address]VALUES('742 Evergreen Terrace','','Springfield','IL','USA','65619',newid(),SYSDATETIME())
 END
-GO
